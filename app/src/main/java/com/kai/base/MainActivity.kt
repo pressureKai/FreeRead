777167fdfc425eb89d77a65ca43e2b4b4c837a68
pressureKai <2466129279@qq.com>
2021-01-02 00:25:36 +0800

    change event bus

--- app/src/main/java/com/kai/base/MainActivity.kt
+++ app/src/main/java/com/kai/base/MainActivity.kt
@@ -1,9 +1,20 @@
 package com.kai.base
 
 import com.kai.base.activity.BaseActivity
+import com.kai.base.eventBusEntity.EventBusEntity
 
 class MainActivity : BaseActivity() {
     override fun setContentView(): Int {
         return R.layout.activity_main
+    }
+
+    override fun initView() {
+        super.initView()
+        postStickyEvent("")
+    }
+
+
+    override fun <String> onMessageReceiver(eventBusEntity: EventBusEntity<String>) {
+        super.onMessageReceiver(eventBusEntity)
     }
 }
--- app/src/main/java/com/kai/base/activity/BaseActivity.kt
+++ app/src/main/java/com/kai/base/activity/BaseActivity.kt
@@ -2,23 +2,88 @@ package com.kai.base.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.kai.base.eventBusEntity.EventBusEntity
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 abstract class BaseActivity : AppCompatActivity() {
+    private var startEventBus = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         init()
+        initView()
     }
 
 
     private fun init(){
         this.setContentView(setContentView())
+        startEventBus = startEventBus()
+        if(startEventBus){
+            if(!EventBus.getDefault().isRegistered(this)){
+                EventBus.getDefault().register(this)
+            }
+        }
+    }
+    open fun initView(){
+
     }
 
 
     abstract fun setContentView() :Int
+    open fun startEventBus():Boolean{
+        return true
+    }
 
 
+    override fun onResume() {
+        super.onResume()
+        if(startEventBus){
+            if(!EventBus.getDefault().isRegistered(this)){
+                EventBus.getDefault().register(this)
+            }
+        }
+    }
 
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if(startEventBus){
+            if(EventBus.getDefault().isRegistered(this)){
+                EventBus.getDefault().unregister(this)
+            }
+        }
+    }
+
+
+    /**
+     *
+     * */
+    @Subscribe(threadMode = ThreadMode.ASYNC,sticky = true)
+    open fun <T> onMessageEvent(eventBusEntity :EventBusEntity<T>){
+        if(eventBusEntity.message == this::class.java.name){
+            EventBus.getDefault().removeStickyEvent(eventBusEntity)
+            onMessageReceiver(eventBusEntity)
+        }
+    }
+
+    open fun <T> onMessageReceiver(eventBusEntity: EventBusEntity<T>){
+
+    }
+
+
+    fun <T> postStickyEvent(data :T,code :Int ?= 0,message :String ?= ""){
+        val eventBusEntity = EventBusEntity<T>()
+        eventBusEntity.data = data
+        eventBusEntity.code = code!!
+        if(message!!.isNotEmpty()){
+            eventBusEntity.message = message
+        }else{
+            eventBusEntity.message = this::class.java.name
+        }
+
+        EventBus.getDefault().postSticky(eventBusEntity)
+    }
 
 
 
