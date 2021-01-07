aca0a52f3dd58b832104eff92dfa896a396419e3
pressureKai <2466129279@qq.com>
2021-01-07 17:51:43 +0800

    BaseFragment.kt add implements immersionOwner

--- app/src/main/java/com/kai/base/fragment/BaseFragment.kt
+++ app/src/main/java/com/kai/base/fragment/BaseFragment.kt
@@ -1,35 +1,45 @@
 package com.kai.base.fragment
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.gyf.immersionbar.components.ImmersionOwner
+import com.gyf.immersionbar.components.ImmersionProxy
 import com.kai.base.mvp.base.BasePresenter
 import com.kai.base.mvp.base.IView
 
-abstract class BaseFragment<P:BasePresenter<Fragment>>:Fragment(), IView {
+abstract class BaseFragment<P : BasePresenter<Fragment>>:Fragment(), IView,ImmersionOwner {
     private var mPresenter :P ?= null
     lateinit var mRootView :View
     private var isCreate = false
     private var hasLoad = false
     private var isVisibleToUser = false
+    private val mImmersionProxy :ImmersionProxy = ImmersionProxy(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         bindView()
         super.onCreate(savedInstanceState)
+        mImmersionProxy.onCreate(savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        mImmersionProxy.onActivityCreated(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return getView(inflater,container,savedInstanceState)
+        return getView(inflater, container, savedInstanceState)
     }
 
 
     private fun getView(inflater: LayoutInflater,
                         container: ViewGroup?,
                         savedInstanceState: Bundle?):View{
-       mRootView = inflater.inflate(setLayoutId(),container,false)
+       mRootView = inflater.inflate(setLayoutId(), container, false)
        isCreate = true
-       lazyLoad(mRootView,savedInstanceState)
+       lazyLoad(mRootView, savedInstanceState)
        return mRootView
     }
     override fun bindView() {
@@ -42,18 +52,41 @@ abstract class BaseFragment<P:BasePresenter<Fragment>>:Fragment(), IView {
     }
 
 
+    override fun onResume() {
+        super.onResume()
+        mImmersionProxy.onResume()
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        mImmersionProxy.onPause()
+    }
     override fun onDestroy() {
         super.onDestroy()
         unBindView()
+        mImmersionProxy.onDestroy()
+    }
+
+
+    override fun onHiddenChanged(hidden: Boolean) {
+        super.onHiddenChanged(hidden)
+        mImmersionProxy.onHiddenChanged(hidden)
+    }
+
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        mImmersionProxy.onConfigurationChanged(newConfig)
     }
 
 
 
-    private fun lazyLoad(view:View,savedInstanceState: Bundle?){
+    private fun lazyLoad(view: View, savedInstanceState: Bundle?){
         if(!isCreate || hasLoad || !isVisibleToUser){
            return
         }
-        lazyInit(view,savedInstanceState)
+        lazyInit(view, savedInstanceState)
     }
 
     override fun setUserVisibleHint(isVisibleToUser: Boolean) {
@@ -62,11 +95,49 @@ abstract class BaseFragment<P:BasePresenter<Fragment>>:Fragment(), IView {
             return
         }else{
             this.isVisibleToUser = isVisibleToUser
-            lazyLoad(mRootView,null)
+            lazyLoad(mRootView, null)
         }
+        mImmersionProxy.isUserVisibleHint = isVisibleToUser
     }
 
     abstract fun createPresenter():P?
     abstract fun setLayoutId():Int
-    abstract fun lazyInit(view:View,savedInstanceState: Bundle?)
+    abstract fun lazyInit(view: View, savedInstanceState: Bundle?)
+
+
+    /**
+     * 懒加载，在view初始化完成之前执行
+     * On lazy after view.
+     */
+    override fun onLazyBeforeView() {}
+
+    /**
+     * 懒加载，在view初始化完成之后执行
+     * On lazy before view.
+     */
+    override fun onLazyAfterView() {}
+
+    /**
+     * Fragment用户可见时候调用
+     * On visible.
+     */
+    override fun onVisible() {}
+
+    /**
+     * Fragment用户不可见时候调用
+     * On invisible.
+     */
+    override fun onInvisible() {}
+
+    /**
+     * 是否可以实现沉浸式，当为true的时候才可以执行initImmersionBar方法
+     * Immersion bar enabled boolean.
+     *
+     * @return the boolean
+     */
+    override fun immersionBarEnabled(): Boolean {
+        return true
+    }
+
+
 }
