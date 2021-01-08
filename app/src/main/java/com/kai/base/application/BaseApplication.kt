2e26cfbc6ba6cbc4b488a138291633d87185d197
pressureKai <2466129279@qq.com>
2021-01-08 11:24:44 +0800

    add bugly crash report and tinker update

--- app/src/main/java/com/kai/base/application/BaseApplication.kt
+++ app/src/main/java/com/kai/base/application/BaseApplication.kt
@@ -1,15 +1,49 @@
 package com.kai.base.application
 
+import android.annotation.TargetApi
 import android.app.Application
+import android.app.Application.ActivityLifecycleCallbacks
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.multidex.MultiDex
 import com.kai.base.utils.LogUtils
+import com.tencent.bugly.Bugly
+import com.tencent.tinker.entry.DefaultApplicationLike
 
-class BaseApplication :Application(){
+
+class BaseApplication(
+    application: Application, tinkerFlags: Int,
+    tinkerLoadVerifyFlag: Boolean, applicationStartElapsedTime: Long,
+    applicationStartMillisTime: Long, tinkerResultIntent: Intent
+) : DefaultApplicationLike(
+    application,
+    tinkerFlags,
+    tinkerLoadVerifyFlag,
+    applicationStartElapsedTime,
+    applicationStartMillisTime,
+    tinkerResultIntent
+) {
     override fun onCreate() {
         super.onCreate()
-        Constant.init(this)
+        Constant.init(application)
         initModelsSpeed()
         initModelsLow()
+        initBugly()
     }
+
+    override fun onBaseContextAttached(base: Context?) {
+        super.onBaseContextAttached(base)
+        MultiDex.install(base)
+        initTinker()
+    }
+
+    private fun initTinker(){
+
+    }
+
+
+
 
     //反射获取BaseInit（子模块中在application 初始化时的操作类统一在BaseApplication执行）
     private fun initModelsSpeed(){
@@ -17,13 +51,13 @@ class BaseApplication :Application(){
             try {
                 val clazz =  Class.forName(init)
                 val moduleInit = clazz.newInstance() as BaseAppInit
-                moduleInit.onInitSpeed(this)
-            }catch (e :ClassNotFoundException){
-                LogUtils.e("BaseApplication","class not found $e")
-            }catch (e :IllegalAccessException){
-                LogUtils.e("BaseApplication","illegal access $e")
-            }catch (e :InstantiationException){
-                LogUtils.e("BaseApplication","instantiation $e")
+                moduleInit.onInitSpeed(application)
+            }catch (e: ClassNotFoundException){
+                LogUtils.e("BaseApplication", "class not found $e")
+            }catch (e: IllegalAccessException){
+                LogUtils.e("BaseApplication", "illegal access $e")
+            }catch (e: InstantiationException){
+                LogUtils.e("BaseApplication", "instantiation $e")
             }
         }
     }
@@ -33,14 +67,25 @@ class BaseApplication :Application(){
             try {
                 val clazz =  Class.forName(init)
                 val moduleInit = clazz.newInstance() as BaseAppInit
-                moduleInit.onInitLow(this)
-            }catch (e :ClassNotFoundException){
-                LogUtils.e("BaseApplication","class not found $e")
-            }catch (e :IllegalAccessException){
-                LogUtils.e("BaseApplication","illegal access $e")
-            }catch (e :InstantiationException){
-                LogUtils.e("BaseApplication","instantiation $e")
+                moduleInit.onInitLow(application)
+            }catch (e: ClassNotFoundException){
+                LogUtils.e("BaseApplication", "class not found $e")
+            }catch (e: IllegalAccessException){
+                LogUtils.e("BaseApplication", "illegal access $e")
+            }catch (e: InstantiationException){
+                LogUtils.e("BaseApplication", "instantiation $e")
             }
         }
+    }
+
+
+    private fun initBugly(){
+        Bugly.init(application, "900029763", Constant.isDebug);
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    fun registerActivityLifecycleCallback(callbacks: ActivityLifecycleCallbacks?) {
+        application.registerActivityLifecycleCallbacks(callbacks)
     }
 }
---
+++ app/src/main/java/com/kai/base/application/TinkerAppLike.kt
@@ -0,0 +1,19 @@
+package com.kai.base.application
+
+import com.tencent.tinker.loader.app.TinkerApplication
+import com.tencent.tinker.loader.shareutil.ShareConstants
+
+/**
+ *
+ * @ProjectName:    My Application
+ * @Description:     java类作用描述
+ * @Author:         pressureKai
+ * @UpdateDate:     2021/1/8 10:42
+ */
+class TinkerAppLike :TinkerApplication(
+    ShareConstants.TINKER_ENABLE_ALL,
+    "com.kai.base.application.BaseApplication",
+    "com.tencent.tinker.loader.TinkerLoader",
+    false
+){
+}
