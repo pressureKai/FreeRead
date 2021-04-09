b486408b2b040dfc1fca55d4ebbf3d1f3977bb39
pressureKai <2466129279@qq.com>
2021-04-09 12:12:03 +0800

    update before dinner

---
+++ app/src/main/java/com/kai/ui/register/RegisterContract.kt
@@ -0,0 +1,21 @@
+package com.kai.ui.register
+
+import com.kai.base.mvp.base.IView
+
+/**
+ *
+ * @ProjectName:    APP-bookPage
+ * @Description:    契约类-注册
+ * @Author:         pressureKai
+ * @UpdateDate:     2021/3/24 10:42
+ */
+class RegisterContract {
+    interface View :IView{
+
+
+    }
+
+    interface Presenter{
+
+    }
+}
